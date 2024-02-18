d36d732b93348f4bb1739f8486a49732de3aa67e
Tatsufumi Haneda <haneda.tatsufumi@isid.co.jp>
2024-02-18 11:47:10 +0900

    modified:   package.json 	modified:   server/index.js 	new file:   server/package.json

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const config = require('./config/dev');
 const FakeDB = require('./fake-db');
 const SampleDb = require('./sample-db');
 const productRoutes = require('./routes/products');
+const path = require('path');
 
 mongoose.connect(config.DB_URI)
 .catch(error => console.log(error))
@@ -23,6 +24,12 @@ const app = express();
 // });
 
 app.use('/api/v1/products', productRoutes);
+
+const appPath = path.join(__dirname, '..', 'dist', 'reservation-app', 'browser');
+app.use(express.static(appPath));
+app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 
